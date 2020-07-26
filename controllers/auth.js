a70b1c6dfcfa407a3d6a4e0bf4047f809b4fbc13
Tomas Melone <tomasmelone@hotmail.com>
2020-07-26 04:02:32 +0200

    Fin seccion 10 Auth

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+const { response } = require("express");
+const Usuario = require("../models/usuario")
+const bcrypt = require("bcryptjs");
+const { generarJWT } = require("../helpers/jwt");
+
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        //Verificar email
+        const usuarioDB = await Usuario.findOne({email});
+        
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: "Datos ingresados incorrectos"
+            });
+        }
+
+        //Verificar contraseña
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        
+        if(!validPassword) {
+
+            return res.status(400).json({
+                ok: false,
+                msg: "Datos ingresados incorrectos"
+            });
+        }
+
+        //Generar el TOKEN - JWT
+        const token = await generarJWT(usuarioDB._id);
+
+
+        res.json({
+            ok: true,
+            token
+        });
+        
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Error inesperado"
+        });
+        
+    }
+}
+
+module.exports = {
+    login
+}
--- index.js
+++ index.js
@@ -11,15 +11,14 @@ const app = express(); //Crear el servidor express
 //CORS Configuration
 app.use( cors() );
 
+//Lectura y parseo del body
+app.use( express.json() );
+
 dbConnection();
 
 //Rutas
-app.get("/", (req,res) => {
-    res.json({
-        ok: true,
-        msg: "Hola Mundo!"
-    });
-});
+app.use("/api/usuarios", require("./routes/usuarios") );
+app.use("/api/login", require("./routes/auth") );
 
 app.listen(process.env.PORT, () => {
     console.log("Servidor running on port " + process.env.PORT);
